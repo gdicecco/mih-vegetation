fde440bf3d0ef32b1f40e864bb7f4494244b9b90
ssnell6 <ssnell@ad.unc.edu>
2019-04-24 12:49:42 -0400

    warbler work

--- scripts/mih_null_mods.R
+++ scripts/mih_null_mods.R
@@ -15,84 +15,16 @@ tax_code2 = tax_code1[-grep("sp.", tax_code1$CRC_SCI_NAME),]
 
 
 troph_AOU <- left_join(troph_guild, tax_code2, by = c("Species" = "CRC_SCI_NAME"))
+troph_AOU$SCI_NAME = gsub('Dendroica','Setophaga', troph_AOU$SCI_NAME)
+
 bbs_sub2 <- filter(bbs_sub1, aou %in% tax_code2$AOU_OUT)
 bbs_troph <- left_join(bbs_sub2, troph_AOU, by = c("aou" = "AOU_OUT"))
 
-
 test <- filter(bbs_troph, is.na(Species) == TRUE)
-tcode <- left_join(test, tax_code[,c("AOU_OUT", "PRIMARY_COM_NAME")], by = c("aou"="AOU_OUT")) 
-sp_nocodes <- unique(tcode$PRIMARY_COM_NAME)
+tcode <- left_join(test, tax_code2[,c("AOU_OUT", "PRIMARY_COM_NAME")], by = c("aou"="AOU_OUT")) %>%
+  select(aou, PRIMARY_COM_NAME.y)
+sp_nocodes <- unique(tcode)
 # tax_code$CRC_SCI_NAME[tax_code$CRC_SCI_NAME == "Dendragapus obscurus/fuliginosus"] <- "Dendragapus obscurus"
-
-####
-sp_list$match = as.character(sp_list$SciName)
-# renaming to get latest scientific names for mismatch spp
-sp_list$match[sp_list$match =="Oreothlypis peregrina"] = "Vermivora peregrina"
-
-sp_list$match[sp_list$match =="Vermivora pinus"] = "Vermivora cyanoptera"
-
-sp_list$match[sp_list$match =="Stellula calliope"] = "Selasphorus calliope"
-
-sp_list$match = gsub('Setophaga ', 'Dendroica ', sp_list$match)
-
-sp_list$match[sp_list$match =="Dendroica ruticilla"] = "Setophaga ruticilla"
-
-sp_list$match[sp_list$match =="Picoides nuttallii"] = "Dryobates nuttallii"
-
-sp_list$match[sp_list$match =="Cardellina canadensis"] = "Wilsonia canadensis"
-
-sp_list$match[sp_list$match =="Geothlypis philadelphia"] = "Oporornis philadelphia"
-
-sp_list$match[sp_list$match =="Oreothlypis ruficapilla"] = "Vermivora ruficapilla"
-
-sp_list$match[sp_list$match =="Oreothlypis celata"] = "Vermivora celata"
-
-sp_list$match[sp_list$match =="Cardellina pusilla"] = "Wilsonia pusilla"
-
-sp_list$match[sp_list$match =="Oreothlypis virginiae"] = "Vermivora virginiae"
-
-sp_list$match[sp_list$match =="Poecile hudsonica"] = "Parus hudsonicus"
-
-sp_list$match[sp_list$match =="Pica hudsonia"] = "Pica pica"
-
-sp_list$match = gsub('Poecile ', 'Parus ', sp_list$match)
-
-sp_list$match[sp_list$match =="Dendroica citrina"] = "Wilsonia citrina"
-
-sp_list$match[sp_list$match =="Geothlypis formosus"] = "Oporornis formosus"
-
-sp_list$match[sp_list$match =="Oreothlypis luciae"] = "Vermivora luciae"
-
-sp_list$match[sp_list$match =="Geothlypis tolmiei"] = "Oporornis tolmiei"
-
-sp_list$match[sp_list$match =="Calcarius mccownii"] = "Rhynchophanes mccownii"
-
-sp_list$match[sp_list$match =="Picoides villosus"] = "Leuconotopicus villosus"
-
-sp_list$match[sp_list$match =="Picoides pubescens"] = "Dryobates pubescens"
-
-sp_list$match[sp_list$match =="Picoides dorsalis"] = "Picoides tridactylus"
-
-sp_list$match[sp_list$match =="Picoides scalaris"] = "Dryobates scalaris"
-
-sp_list$match[sp_list$match =="Picoides albolarvatus"] = "Leuconotopicus albolarvatus"
-
-sp_list$match[sp_list$match =="Picoides borealis"] = "Leuconotopicus borealis"
-
-sp_list$match[sp_list$match =="Aimophila cassinii"] = "Peucaea cassinii"
-
-sp_list$match[sp_list$match =="Aimophila aestivalis"] = "Peucaea aestivalis"
-
-sp_list$match[sp_list$match =="Aimophila botterii"] = "Peucaea botterii"
-
-sp_list$match[sp_list$match =="Aimophila carpalis"] = "Peucaea carpalis"
-
-sp_list$match[sp_list$match =="Oreothlypis crissalis"] = "Vermivora crissalis"
-
-sp_list$match[sp_list$match =="Ixoreus naevius"] = "Zoothera naevia"
-
-
-
 
 
 gimms_agg = gimms_ndvi %>% filter(month == c("may", "jun", "jul")) %>% 
