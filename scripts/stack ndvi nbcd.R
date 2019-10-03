5ae13e6ffe538260b7b5437a0d05cf1585bf0d93
ssnell6 <ssnell@ad.unc.edu>
2019-10-03 15:49:02 -0400

    working on stakcing in LL

--- scripts/stack ndvi nbcd.R
+++ scripts/stack ndvi nbcd.R
@@ -1,16 +1,9 @@
-library(tidyverse)
-library(cowplot)
 library(rgdal)
 library(raster)
-library(gimms)
-library(sf)
-library(sp)
-library(tmap)
 
 prj.string <- "+proj=laea +lat_0=45.235 +lon_0=-106.675 +units=km"
 
-setwd("C:/git/mih-vegetation")
-nlcd_2001 <- raster("Z:/GIS/LandCoverData/nlcd_2001_landcover_2011_edition_2014_10_10/nlcd_2001_landcover_2011_edition_2014_10_10.img") 
+nlcd_2001 <- raster("/proj/hurlbertlab/nlcd_landcover/nlcd_2001_landcover_2011_edition_2014_10_10/nlcd_2001_landcover_2011_edition_2014_10_10.img") 
 nlcd_2001 <-projectRaster(nlcd_2001, crs = prj.string)
   
 lat_long = read.csv("data/latlongs.csv", header = TRUE)
