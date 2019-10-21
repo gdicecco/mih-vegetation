88b68630e096afda18285e7ea4684e8a7b08e0f3
Di Cecco <gdicecco@ad.unc.edu>
2019-10-21 17:03:52 -0400

    CHange color of BBS dots on map

--- scripts/Figure1_maps.R
+++ scripts/Figure1_maps.R
@@ -9,7 +9,7 @@ library(rgdal)
 library(raster)
 
 
-us_routes <- read_sf("Z:/Databases/BBS/GPS_stoplocations/bbsrte_2012_alb/bbsrte_2012_alb.shp")
+us_routes <- read_sf("\\\\BioArk\\hurlbertlab/Databases/BBS/GPS_stoplocations/bbsrte_2012_alb/bbsrte_2012_alb.shp")
 us <- tm_shape(us_routes) + tm_borders() + tm_fill(col = "light gray")
 
 # Read in BBS
@@ -20,6 +20,30 @@ bbs_plot_rtes <- filter(us_routes, rteno %in% final.counts$stateroute)
 bbc_censuses <- read.csv("data/bbc_censuses.csv", stringsAsFactors = F)
 bbc_counts <- read.csv("data/bbc_counts.csv", stringsAsFactors = F)
 bbc_sites <- read.csv("data/bbc_sites.csv", stringsAsFactors = F)
+
+# Get species list - diurnal land birds
+species_list <- read.csv("data/species_list.csv", stringsAsFactors = F)
+
+# Match species common names to BBS species list
+fix_spp <- list(new_species = c("Sage Sparrow" = "Sagebrush Sparrow", 
+                                "Western Scrub Jay" = "California Scrub Jay", 
+                                "Sharp-tailed Sparrow" = "Nelson's Sparrow", 
+                                "Common Crackle" = "Common Grackle", 
+                                "Three-toed Woodpecker" = "American Three-toed Woodpecker", 
+                                "Yellow-rumped Warbler" = "(unid. Myrtle/Audubon's) Yellow-rumped Warbler", 
+                                "Rock Dove" = "Rock Pigeon", 
+                                "Northern Oriole" = "Baltimore Oriole", 
+                                "Plain Titmouse" = "unid. Oak Titmouse / Juniper Titmouse", 
+                                "Scrub Jay" = "California Scrub Jay", 
+                                "Northern Flicker" = "(unid. Red/Yellow Shafted) Northern Flicker", 
+                                "Western Flycatcher" = "unid. Cordilleran / Pacific-slope Flycatcher", 
+                                "Solitary Vireo" = "unid. Cassin's Vireo / Blue-headed Vireo", 
+                                "Dark-eyed Junco" = "(unid. race) Dark-eyed Junco", 
+                                "Brown Towhee" = "California Towhee", 
+                                "Rufous-sided Towhee" = "unid. Spotted Towhee / Eastern Towhee"))
+
+new_spp_names <- as.data.frame(fix_spp)
+new_spp_names$common_names <- row.names(new_spp_names)
 
 # Most recent Census for each site
 site_census <- bbc_censuses %>%
@@ -61,7 +85,7 @@ bbc_sf <- bbc_censuses %>%
   dplyr::select(siteID, sitename, latitude, longitude, year, area, richness) %>%
   right_join(site_census, by = c("siteID", "year")) %>%
   mutate_at("longitude", .funs = ~{.*-1}) %>%
-  st_as_sf(coords = c("longitude", "latitude"), crs = st_crs(us_states)) %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = "+proj=longlat +datum=WGS84 +no_defs") %>%
   group_by(siteID, area) %>%
   summarize(nCensus = length(unique(year)), 
             censusYear = max(year),
@@ -76,7 +100,7 @@ bbc_sf <- bbc_censuses %>%
 
 box <- c(xmin = -150, xmax = -65, ymin = 25, ymax = 50)
 
-northAM <- read_sf("Z:/Snell/MIH/ne_50m_admin_1_states_provinces_lakes.shp")
+northAM <- read_sf("data/ne_50m_admin_1_states_provinces_lakes.shp")
 eNA <- st_crop(northAM, box)
 niche_compx_routes <- read_sf("data/niche_complexity_mod_routes.shp")
   
@@ -85,9 +109,11 @@ us <- tm_shape(eNA) + tm_borders() + tm_fill(col = "#d9d9d9")
 bbc_map <- us + tm_shape(bbc_sf) + 
   tm_dots(col = "nCensus", alpha = 1, size = 1, palette = "GnBu", title = "Number of Censuses")
 
-point_map <- us + tm_shape(bbs_plot_rtes) + tm_lines(lwd = 2, col = "black") + tm_shape(niche_compx_routes) + tm_dots(col = "red", size = 0.1) + tm_shape(bbc_sf) + tm_dots(col = "#0c2c84", size =0.2)+ 
+point_map <- us + tm_shape(bbs_plot_rtes) + tm_lines(lwd = 2, col = "black") + 
+  tm_shape(bbc_sf) + tm_dots(col = "springgreen3", size = 0.2)+ 
   tm_add_legend(type = c("line"), labels = c("BBS"),lwd = 2, size = 42, col = "black") +
-  tm_add_legend(type = c("symbol"), labels = c(" BBC"), shape = 16,col = "#0c2c84", size = 42) +
+  tm_add_legend(type = c("symbol"), labels = c(" BBC"), shape = 16, col = "springgreen3", size = 42) +
+  tm_shape(niche_compx_routes) + tm_dots(col = "red", size = 0.1) +
   tm_add_legend(type = c("symbol"), labels = c(" Niche Complexity"), shape = 16,col = "red", size = 42)
 tmap_save(point_map, "Figures/Figure1.pdf")
 
