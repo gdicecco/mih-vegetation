a6fbecb2eeb9d0b75f717969a804985a136e4088
Di Cecco <gdicecco@ad.unc.edu>
2019-11-22 10:34:24 -0500

    Resolve species lists between EltonTraits and BBS AOUs

--- scripts/functional_niche_specialization_BBS.R
+++ scripts/functional_niche_specialization_BBS.R
@@ -13,26 +13,36 @@ BL_checklist <- read.csv("\\\\BioArk//HurlbertLab//DiCecco//data//BirdLife_Check
 
 birdfunc <- read.table("\\\\BioArk//HurlbertLab//Databases//Elton Traits//BirdFuncDat.txt", sep = "\t", header = T, quote = "\"", stringsAsFactors = F)
 
-bbs_func <- species_list %>%
-  filter(!grepl("unid", english_common_name)) %>%
+family_species_join <- birdfunc %>%
+  mutate(species = word(Scientific, 2, sep = " ")) %>%
+  right_join(species_list, by = c("BLFamilyLatin" = "family", "species" = "species"))
+
+binomial_join <- bbs_func %>%
+  filter(is.na(Diet.5Cat)) %>%
+  filter(!grepl("unid.", english_common_name)) %>%
+  dplyr::select(genus, species, english_common_name, aou) %>%
   mutate(binomial = paste(genus, species, sep = " ")) %>%
-  left_join(birdfunc, by = c("binomial" =  "Scientific"))
+  left_join(birdfunc, by = c("binomial" = "Scientific"))
 
-mismatched_spp <- bbs_func %>%
-  filter(is.na(Diet.5Cat))
+common_name_join <- mismatched_spp %>%
+  filter(is.na(Diet.5Cat)) %>%
+  dplyr::select(genus, species, english_common_name, aou, binomial) %>%
+  left_join(birdfunc, by = c("english_common_name" = "English"))
+# Anything that's left is subspecies or Eastern Yellow Wagtail (Asian spp found in AK)
 
-## Match common names with old BirdLife Checklist (V3) to consolidate mismatches from genera; tricolored vs. tricoloured blackbird
+# BBS species traits with AOUs
 
-BL_spp <- species_list %>%
-  mutate(binomial = paste(genus, species, sep = " ")) %>%
-  filter(!grepl("unid", english_common_name)) %>%
-  left_join(BL_checklist, by = c("binomial" = "Scientific_name"))
+family_spp_matches <- family_species_join %>%
+  filter(!is.na(Diet.5Cat)) %>%
+  mutate(binomial = paste(genus, species, sep = " "))
 
-find_synonyms <- function(species) {
-  BL_checklist %>%
-  filter(str_detect(species, Synonyms))
-}
+binomial_matches <- binomial_join %>%
+  filter(!is.na(Diet.5Cat))
 
-possibly_find_synonyms <- possibly(find_synonyms, NA)
+common_name_matches <- binomial_join %>%
+  filter(!is.na(Diet.5Cat))
 
-
+bbs_func <- family_spp_matches %>%
+  dplyr::select(-french_common_name, -spanish_common_name, -species_id, -sporder) %>%
+  bind_rows(binomial_matches) %>%
+  bind_rows(common_name_matches)
