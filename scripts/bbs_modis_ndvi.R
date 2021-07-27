cc445ab7eb8b1a2dd10d6ef5bb1ff4012b11ced4
gdicecco <gdicecco@live.unc.edu>
2021-07-27 15:35:20 -0400

    partial BBS modis NDVI

--- scripts/bbs_modis_ndvi.R
+++ scripts/bbs_modis_ndvi.R
@@ -19,9 +19,9 @@ bbs_modis_years <- c(2000:2004)
 ## MODIS NDVI: May 1-July 31
 # MOD13Q1
 
-bands <- mt_bands(product = "MOD13Q1")
-ndvi_band <- bands$band[grep("NDVI", bands$band)]
-evi_band <- bands$band[grep("EVI", bands$band)]
+modis_bands <- mt_bands(product = "MOD13Q1")
+ndvi_band <- modis_bands$band[grep("NDVI", modis_bands$band)]
+evi_band <- modis_bands$band[grep("EVI", modis_bands$band)]
 
 
 ## Extract MODIS data
@@ -29,46 +29,48 @@ evi_band <- bands$band[grep("EVI", bands$band)]
 bbs_modis_ndvi <- vector(mode = "list", length = length(bbs_modis_years))
 bbs_modis_evi <- vector(mode = "list", length = length(bbs_modis_years))
 
-for(i in 1:length(bbs_modis_years)) {
+for(i in 3:length(bbs_modis_years)) {
   y <- bbs_modis_years[i]
   
-  modis_ndvi <- mt_batch_subset(df = bbs_sites[1, ],
+  modis_ndvi <- mt_batch_subset(df = bbs_sites[1:950, ],
                                 product = "MOD13Q1",
                                 start = paste0(y, "-05-01"),
                                 end = paste0(y, "-07-31"),
                                 band = ndvi_band)
   
-  for(j in 2:nrow(bbs_sites)) {
-    res <- mt_batch_subset(df = bbs_sites[j, ],
-                                  product = "MOD13Q1",
-                                  start = paste0(y, "-05-01"),
-                                  end = paste0(y, "-07-31"),
-                                  band = ndvi_band)
-    
-    modis_ndvi <- rbind(modis_ndvi, res)
-  }
-  
+  # for(j in 2:nrow(bbs_sites)) {
+  #   res <- mt_batch_subset(df = bbs_sites[j, ],
+  #                                 product = "MOD13Q1",
+  #                                 start = paste0(y, "-05-01"),
+  #                                 end = paste0(y, "-07-31"),
+  #                                 band = ndvi_band)
+  #   
+  #   modis_ndvi <- rbind(modis_ndvi, res)
+  # }
+  # 
   
   bbs_modis_ndvi[[i]] <- modis_ndvi
   
-  modis_evi <- mt_batch_subset(df = bbs_sites[1, ],
+  modis_evi <- mt_batch_subset(df = bbs_sites[1:800, ],
                                product = "MOD13Q1",
                                start = paste0(y, "-05-01"),
                                end = paste0(y, "-07-31"),
                                band = evi_band)
   
-  for(k in 2:nrow(bbs_sites)) {
-    res2 <- mt_batch_subset(df = bbs_sites[k, ],
-                           product = "MOD13Q1",
-                           start = paste0(y, "-05-01"),
-                           end = paste0(y, "-07-31"),
-                           band = evi_band)
-    
-    modis_evi <- rbind(modis_evi, res2)
-  }
+  # for(k in 2:nrow(bbs_sites)) {
+  #   res2 <- mt_batch_subset(df = bbs_sites[k, ],
+  #                          product = "MOD13Q1",
+  #                          start = paste0(y, "-05-01"),
+  #                          end = paste0(y, "-07-31"),
+  #                          band = evi_band)
+  #   
+  #   modis_evi <- rbind(modis_evi, res2)
+  # }
     
   
   bbs_modis_evi[[i]] <- modis_evi
+  
+  print(paste(Sys.time(), y))
 }
 
 modis_ndvi_df <- do.call(rbind.data.frame, bbs_modis_ndvi)
